c4962a6f0515a1f5c1a4ec774df05dd8462514ee
김하영 <khy4018@gmail.com>
2023-02-14 16:38:35 +0900

    :lipstick: Add base css

---
+++ src/utility/theme.js
@@ -0,0 +1,25 @@
+
+export const colorChip = {
+  primary: '#3147eb',
+  secondary: '#ebd531',
+  sky: '#31a4eb',
+  purple: '#7831eb',
+  pink: '#d531eb',
+  red: '#eb3147',
+  gray: 'rgba(255, 255, 255, .2)',
+}
+
+export const bgColors = {
+  dark: '#242424',
+  light: '#4c4c4c'
+}
+
+export const text = {
+  lightGray: '#eee'
+}
+
+export const darkTheme = {
+  ...colorChip,
+  bgColors,
+  text
+}
