52d8868012c8b706a958c04be8fba2182133bd26
김하영 <khy4018@gmail.com>
2023-02-14 16:39:08 +0900

    :art: Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import "./assets/styles/App.module.scss"
 import {RecoilRoot} from 'recoil'
 import {BrowserRouter} from 'react-router-dom'
 import Router from './router'
+import {ThemeProvider} from 'styled-components'
+import {useState} from 'react'
+import {darkTheme} from './utility/theme'
 
 function App() {
+  const [theme, setTheme] = useState(darkTheme)
+
   return (
     <RecoilRoot>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ThemeProvider theme={{...theme, setTheme}}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
