dac4babd6a7080ff11e3d9fefda6d42bf56c466b
김하영 <khy4018@gmail.com>
2023-02-14 18:33:57 +0900

    :seedling: Create user data

---
+++ src/database/users.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: 0,
+    loginId: 'hayeong',
+    password: '123123123',
+    phoneNumber: '010-6213-6178',
+    profileImg: require('../assets/images/user--0.jpg')
+  },
+  {
+    id: 1,
+    loginId: 'onew',
+    password: '123123123',
+    phoneNumber: '010-1234-1234',
+    profileImg: require('../assets/images/user--1.jpg')
+  }
+]
