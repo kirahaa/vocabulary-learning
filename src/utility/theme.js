623c763f5f1fa436f9e0f670b603419fb3961f5c
김하영 <khy4018@gmail.com>
2023-02-15 15:24:54 +0900

    :lipstick: Update theme colors

--- src/utility/theme.js
+++ src/utility/theme.js
@@ -2,11 +2,13 @@
 export const colorChip = {
   primary: '#3147eb',
   secondary: '#ebd531',
+  green: '#76ff03',
   sky: '#31a4eb',
   purple: '#7831eb',
   pink: '#d531eb',
   red: '#eb3147',
   gray: 'rgba(255, 255, 255, .2)',
+  dark: '#2c2c2c'
 }
 
 export const bgColors = {
